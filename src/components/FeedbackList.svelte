7f6369a1ecb146db05aa3635840ac624560660e9
unchartedland <86623221+unchartedland@users.noreply.github.com>
2022-02-04 14:56:28 -0500

    Add files via upload

---
+++ src/components/FeedbackList.svelte
@@ -0,0 +1,19 @@
+<script>
+    import { FeedbackStore } from '../stores'
+    import { fade, scale } from "svelte/transition";
+    import FeedbackItem from "./FeedbackItem.svelte";
+    //import { onDestroy, onMount } from 'svelte';
+    // let feedback = []
+
+    // const unsubscribe = FeedbackStore.subscribe((data) => feedback = data)
+
+    // onDestroy(() => {
+    //     unsubscribe()
+    // });
+</script>
+
+{#each $FeedbackStore as fb (fb.id)}
+<div in:scale out:fade="{{ duration: 500 }}">
+    <FeedbackItem item={fb} />
+</div>
+{/each}
